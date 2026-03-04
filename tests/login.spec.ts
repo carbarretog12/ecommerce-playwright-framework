5d8507921e0a41241e31569db271346f4a4cd18c
carbarretog12 <carbarretog12@gmail.com>
2026-03-03 23:22:12 -0500

    refactor: implementar data-driven testing en login con JSON

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,40 +1,31 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
 
-// 0. Importar usuarios de prueba
-import userData from '../data/users.json';
+// Importar usuarios de prueba
+import usuariosData from '../data/users.json';
 
-test('Debe permitir iniciar sesión con un usuario válido', async ({ page }) => {
-  const loginPage = new LoginPage(page);
-  await loginPage.navigate();
+test.describe('Pruebas Dinámicas de Login (Data-Driven)', () => {
 
-  // Test 1: usuario válido. 
-  await loginPage.login(userData.usuarioValido.username, userData.usuarioValido.password);
-  await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+  //Test para usuarios de archivo ../data/users.json
+  for (const usuario of usuariosData) {
+
+    test(`Intento de login con: ${usuario.id}`, async ({ page }) => {
+      const loginPage = new LoginPage(page);
+      await loginPage.navigate();
+      
+      // 1. Intento autenticación
+      await loginPage.login(usuario.username, usuario.password);
+
+      // 2. Validación test según ajuste esperado
+      if (usuario.esperaExito) {
+        // Usuario presuntamente válido
+        await expect(page).toHaveURL(/inventory.html/);
+      } else {
+        // Usuario presuntamente erroneo
+        await expect(loginPage.errorMessage).toBeVisible();
+        await expect(loginPage.errorMessage).toContainText(usuario.mensajeError);
+      }
+    });
+
+  }
 });
-
-test('Debe mostrar error con un usuario bloqueado', async ({ page }) => {
-  const loginPage = new LoginPage(page);
-  await loginPage.navigate();
-
-  // Test 2: usuario bloqueado. 
-  await loginPage.login(userData.usuarioBloqueado.username, userData.usuarioBloqueado.password);
-
-  // Validar mensaje de error para usuario bloqueado
-  await expect(loginPage.errorMessage).toBeVisible();
-  await expect(loginPage.errorMessage).toContainText('Sorry, this user has been locked out');
-});
-
-test('Debe mostrar error de usuario inválido', async ({ page }) => {
-  const loginPage = new LoginPage(page);
-  await loginPage.navigate();
-
-  // Test 2: usuario no válido. 
-  await loginPage.login(userData.usuarioInvalido.username, userData.usuarioInvalido.password);
-
-  // Validar mensaje de error para usuario no válido
-  await expect(loginPage.errorMessage).toBeVisible();
-  await expect(loginPage.errorMessage).toContainText('Epic sadface: Username and password do not match any user in this service');
-});
-//npx playwright codegen https://www.saucedemo.com/ ; Inspeccionar en el sitio las propiedades data-test
-//npx playwright test --project=chromium --headed ; Lanzar todas las pruebas
